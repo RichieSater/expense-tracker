d17ba6c84a4f91a93e8fa1929bf933696fd3b97f
Richie Sater <richiesater@gmail.com>
2021-05-27 21:14:46 -0400

    Set up expense components and  props

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,18 @@
 import "./ExpenseItem.css";
+import Card from "../UI/Card"
+import ExpenseDate from './ExpenseDate'
+
 const ExpenseItem = (props) => {
 
   return (
-    <div className="expense-item">
-      <div>{props.date}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">{props.amount}</div>
-    </div>
+    </Card>
   );
-}
+};
 
 export default ExpenseItem;
